552b35f96c550b54a028e6387b0c794452164e1b
Harshveer Hehar <hhehar4@uwo.ca>
2020-11-23 18:51:56 -0500

    Added DB to store users

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const bodyParser = require('body-parser');
 const validator = require('validator');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB,
+    {useNewUrlParser: true,
+    useUnifiedTopology: true},
+    () => console.log('connected to db') 
+);
 
 const users = require('./routes/users');
 
@@ -11,6 +21,8 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/', express.static(__dirname + '/MyApp')); 
 
 app.use('/users', users);
 
